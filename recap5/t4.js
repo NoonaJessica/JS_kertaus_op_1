6602bf994b81928321197aa3cbe5716da0695005
Noona <noonaposti@gmail.com>
2023-09-01 12:10:32 +0300

    tunti formi eteni

---
+++ recap5/t4.js
@@ -0,0 +1,32 @@
+'use strict';
+
+async function fetchData(url, options) {
+  const respose = await fetch(url, options);
+  if (!respose.ok) {
+    console.log(respose);
+  throw new Error(`Error in request: ${respose.status}`);
+  }
+  const json = await respose.json();
+  return json;
+}
+
+(async function () {
+  try {
+    const user = {
+      name: 'John Doe',
+      job: 'Developer',
+    };
+    const url = 'https://reqres.in/api/unknown/23';
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      'body: JSON.stringify(user),'
+    };
+    const userData = await fetchData(url, options);
+    console.log(userData);
+  } catch (error) {
+    alert(error.message);
+  }
+})();
